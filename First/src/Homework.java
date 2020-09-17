5319f22eb51eb6b5505c12fd55dee33dec6ee626
kpw6 <kpw6@njit.edu>
2020-09-17 19:58:06 -0400

    add Java Data HW

---
+++ First/src/Homework.java
@@ -0,0 +1,22 @@
+public class Homework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String name = "name";
+
+		// 2. Output the name to the console
+		System.out.println(name);
+
+		// 3. Create a variable that holds an age (as a whole number)
+		int age = 42;
+
+		// 4. Output the age to the console
+		System.out.println(age);
+
+		// 5. Create a variable that holds a decimal
+		double decimal = 4.23;
+
+		// 6. Output the variable
+		System.out.println(decimal);
+	}
+}

38670edeec2d65dcaf6175b869f8509413eca2a9
kpw6 <kpw6@njit.edu>
2020-09-21 16:59:31 -0400

    added Hw2 Loops

---
+++ First/src/LoopsHW2.java
@@ -0,0 +1,16 @@
+
+public class LoopsHW2{
+
+	public static void main(String[] args) {
+		int[] numbers = {2, 7, 33, 47, 62, 79, 23};
+		for (int num : numbers)
+		{
+			if (num % 2 == 0)
+			{
+				System.out.print(num + " ");
+			}
+		}
+
+	}
+
+}

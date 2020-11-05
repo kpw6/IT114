6e64ed0e11ba6a61fa42475de56cb66e66b5db10
kpw6 <kpw6@njit.edu>
2020-11-04 20:49:19 -0500

    Sockets

--- Server/src/Server/Payload.java
+++ Server/src/Server/Payload.java
@@ -1,3 +1,4 @@
+package Server;
 import java.io.Serializable;
 
 public class Payload implements Serializable {
---
+++ Server/src/Server/PayloadType.java
@@ -0,0 +1,4 @@
+package Server;
+public enum PayloadType {
+    CONNECT, DISCONNECT, MESSAGE, CLEAR_PLAYERS
+}
